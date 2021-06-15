2ca3b09b4464a84000c7430c95b462f65248c4f3
Parth Gupta <parthgupta2510@gmail.com>
2021-06-15 19:59:42 +0530

    initial  commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function contactViaEmail() {
+            document.getElementById("contact-form").style.display = 'none';
+            document.getElementById("contact-email").style.display = 'block';
+            document.getElementById("label-email").style.backgroundColor = 'rgba(42,42,43,255)'
+            document.getElementById("label-email").style.color = 'rgba(224,208,167,255)'
+            document.getElementById("label-form").style.color = null;
+            document.getElementById("label-form").style.backgroundColor = null;
+}
+
+function contactViaForm() {
+    document.getElementById("contact-form").style.display = 'block';
+    document.getElementById("contact-email").style.display = 'none';
+    document.getElementById("label-form").style.backgroundColor = 'rgba(42,42,43,255)'
+    document.getElementById("label-form").style.color = 'rgba(224,208,167,255)'
+    document.getElementById("label-email").style.color = null;
+    document.getElementById("label-email").style.backgroundColor = null;
+}
+
+function copySpanToClipboard() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("copy-me"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+    alert("Copied the Email Address to clipboard");
+}
